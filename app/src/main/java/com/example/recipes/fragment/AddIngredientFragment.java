404c81168a750c9650975d5e4e05fd98850dcbed
Petru <petre.hertea@gmail.com>
2024-04-11 18:44:53 +0300

    adjusted design

---
+++ app/src/main/java/com/example/recipes/fragment/AddIngredientFragment.java
@@ -0,0 +1,104 @@
+package com.example.recipes.fragment;
+
+import android.os.Bundle;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.recipes.R;
+import com.example.recipes.models.AvailableIngredient;
+import com.example.recipes.mvvm.IngredientViewModel;
+
+import java.util.Objects;
+
+
+public class AddIngredientFragment extends Fragment {
+
+    EditText etName, etQuantity;
+    Button btnSave;
+    IngredientViewModel ingredientViewModel;
+    NavController navController;
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                navController.popBackStack();
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_add_ingredient, container, false);
+
+        navController = Navigation.findNavController(requireActivity(),R.id.nav_host_main);
+
+        etName =view.findViewById(R.id.etName);
+        etQuantity =view.findViewById(R.id.etQuantity);
+        btnSave=view.findViewById(R.id.btnSave);
+
+        ingredientViewModel = new ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(Objects.requireNonNull(this.getActivity()).getApplication())).get(IngredientViewModel.class);
+
+
+        Bundle args=getArguments();
+
+        int id=args.getInt("ingredientID");
+
+        if (id!=-1){
+            getActivity().setTitle("Modifică ingredient");
+            etName.setText(args.getString("name"));
+            etQuantity.setText(String.valueOf(args.getFloat("quantity")));
+            btnSave.setText(R.string.modifica);
+        }
+        else{
+            getActivity().setTitle("Adaugă ingredient");
+        }
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name, quantityText;
+
+                name = etName.getText().toString();
+                quantityText=etQuantity.getText().toString();
+
+                if (name.trim().isEmpty() || quantityText.trim().isEmpty()){
+                    Toast.makeText(getContext(), "Toate câmpurile trebuie completate!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                double quantity=Double.parseDouble(quantityText);
+
+                if (id==-1) {
+                    AvailableIngredient availableIngredient = new AvailableIngredient(name, quantity);
+                    ingredientViewModel.insert(availableIngredient);
+                    navController.popBackStack();
+                    Toast.makeText(getContext(), "Ingredient adăugat!", Toast.LENGTH_SHORT).show();
+                }else{
+                    AvailableIngredient availableIngredient = new AvailableIngredient(name, quantity);
+                    availableIngredient.setIngredientID(id);
+                    ingredientViewModel.update(availableIngredient);
+                    navController.popBackStack();
+                    Toast.makeText(getContext(), "Ingredient modificat!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/recipes/recyclerview/AvailableIngredientAdapter.java
@@ -0,0 +1,72 @@
+package com.example.recipes.recyclerview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.recipes.R;
+import com.example.recipes.models.AvailableIngredient;
+
+public class AvailableIngredientAdapter extends ListAdapter<AvailableIngredient, AvailableIngredientAdapter.IngredientViewHolder> {
+
+
+    public AvailableIngredientAdapter(){
+        super(DIFF_CALLBACK);
+
+    }
+
+
+    public static final DiffUtil.ItemCallback<AvailableIngredient> DIFF_CALLBACK=new DiffUtil.ItemCallback<AvailableIngredient>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull AvailableIngredient oldItem, @NonNull AvailableIngredient newItem) {
+            return oldItem.getIngredientID()== newItem.getIngredientID();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull AvailableIngredient oldItem, @NonNull AvailableIngredient newItem) {
+            return oldItem.getName().equals(newItem.getName()) && oldItem.getQuantity()==newItem.getQuantity();
+        }
+    };
+
+    @NonNull
+    @Override
+    public IngredientViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.available_recipe_item,parent,false);
+        return new IngredientViewHolder(view);
+    }
+
+    public AvailableIngredient getIngredient(int position){
+        return getItem(position);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IngredientViewHolder holder, int position) {
+        AvailableIngredient availableIngredient=getItem(position);
+        holder.name.setText(availableIngredient.getName());
+        holder.quantity.setText(String.valueOf(availableIngredient.getQuantity()));
+
+    }
+
+    public static class IngredientViewHolder extends RecyclerView.ViewHolder{
+
+        TextView name,quantity;
+        ImageView edit, delete;
+
+        public IngredientViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name=itemView.findViewById(R.id.tvIngredientName);
+            quantity=itemView.findViewById(R.id.tvQuantity);
+            edit=itemView.findViewById(R.id.edit);
+            delete=itemView.findViewById(R.id.delete);
+
+        }
+    }
+}
